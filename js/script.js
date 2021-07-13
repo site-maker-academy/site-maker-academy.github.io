4bc50261697fa84529026e9ad1372e695804975e
site-maker-academy <83205812+site-maker-academy@users.noreply.github.com>
2021-07-13 12:40:33 +0300

    Add files via upload
    Обновляет файлы проекта

--- js/script.js
+++ js/script.js
@@ -1,187 +1,152 @@
-/* global generateData getRandomNumber */
+// ********************* урок 9 *********************
+/*Обработчик клика на "+" для появления/скрытия панели элементов */
 
-const currentData = []; // можем подключить utils.js и вызвать generateData() для получения тестовых данных
-const currentLayout = 'landing';
-
-// Пример структуры данных
-// const generatedExample = {
-//   id: '1', // уникальный id
-//   type: 'h1', // типы блоков описаны в BLOCK_TYPES
-//   content: 'Это пример блока H1', // содержимое блока
-//   container: 'header', // контейнеры описаны в CONTAINER_TYPES
-// };
-
-/**
- * Инициализация приложения
- */
-const init = function () {
-  switchLayout(currentLayout);
-  currentData.forEach(renderElement);
-
-  const inputElement = document.querySelector('#grid-' + currentLayout);
-  inputElement.setAttribute('checked', 'checked');
-
-  const addButtonElements = document.querySelectorAll('.add-btn');
-  addButtonElements.forEach(function (item) {
+const showAddMenuHandler = function (evt) {
+  const parentElement = evt.target.parentNode;
+  const addMenuElement = parentElement.querySelector('.choose-elem');
+  addMenuElement.classList.toggle('hidden');
+};  
+const addButtonElements = document.querySelectorAll('.add-btn');
+addButtonElements.forEach(function (item) {
     return item.addEventListener('click', showAddMenuHandler);
   });
 
-  const chooseButtonElements = document.querySelectorAll('.choose-elem__btn');
-  chooseButtonElements.forEach(function (item) {
-    return item.addEventListener('click', addElementHandler);
-  });
 
-  document.querySelector('.grid-select').addEventListener('change', changeLayoutHandler);
-};
 
-/**
- * Создание DOM элемента и вставка в документ на основе данных block
- * @param {object} block
- */
-const renderElement = function (block) {
-  // 1. Выбрать и заполнить шаблон
-  const templateElement = document.querySelector('.' + block.type + '-template').content.cloneNode(true);
-  const contentElement = templateElement.querySelector('.template-content');
 
-  if (block.type === 'img') {
-    contentElement.src = block.content;
-  } else {
-    contentElement.textContent = block.content;
-  }
+// ********************* урок 10 *********************
+/* переключения сетки сайта при изменении переключателя */
 
-  const blockElement = templateElement.firstElementChild;
-  blockElement.dataset.id = block.id; // currentData-id
-
-  // 2. Вставить элемент в контейнер
-  const containerWrapperElement = document.querySelector('.' + block.container + '__elements-wrapper');
-  containerWrapperElement.append(blockElement);
-
-  // 3. Добавить обработчики
-  blockElement.querySelector('.delete-btn').addEventListener('click', buttonDeleteHandler);
-  blockElement.querySelector('.template-content').addEventListener('dblclick', editContentHandler, true);
-
-  // 4. Удаление признака пустого контейнера
-  if (block.container.includes('content')) {
-    containerWrapperElement.parentElement.classList.remove('content--empty');
-  } else {
-    containerWrapperElement.parentElement.classList.remove(block.container + '--empty');
-  }
-};
-
-/**
- * Обработчик удаления элемента
- * @param {MouseEvent} evt
- */
-const buttonDeleteHandler = function (evt) {
-  const element = evt.target.parentNode;
-  const id = +element.dataset.id;
-  const blockIndex = currentData.findIndex(function (item) {
-    return item.id === id;
-  });
-  const block = currentData[blockIndex];
-
-  const wrapper = element.parentNode;
-  element.remove();
-
-  if (wrapper.childNodes.length === 0) {
-    if (block.container.includes('content')) {
-      wrapper.parentNode.classList.add('content--empty');
-    } else {
-      wrapper.parentNode.classList.add(block.container + '--empty');
-    }
-  }
-
-  currentData.splice(blockIndex, 1);
-};
-
-/**
- * Обработчик изменения контента
- * @param {MouseEvent} evt
- */
-const editContentHandler = function (evt) {
-  const editedElement = evt.target;
-  const id = +editedElement.parentNode.dataset.id;
-  const blockIndex = currentData.findIndex(function (item) {
-    return item.id === id;
-  });
-  const block = currentData[blockIndex];
-
-  let currentValue;
-
-  if (editedElement.tagName === 'IMG') {
-    currentValue = editedElement.src;
-  } else {
-    currentValue = editedElement.textContent;
-  }
-
-  const newValue = window.prompt('Вы хотите поменять значение ?', currentValue);
-
-  if (newValue) {
-    if (editedElement.tagName === 'IMG') {
-      editedElement.src = newValue;
-    } else {
-      editedElement.textContent = newValue;
-    }
-
-    block.content = newValue;
-  }
-};
-
-/**
- * Обработчик клика на "+" для добавления блока
- * @param {MouseEvent} evt
- */
-const showAddMenuHandler = function (evt) {
-  const parentElement = evt.currentTarget.parentNode;
-  const addMenuElement = parentElement.querySelector('.choose-elem');
-
-  addMenuElement.classList.toggle('hidden');
-};
-
-const DEFAULT_IMG = 'img/empty.png';
-
-/**
- * Обработчик добавление блока
- * @param {MouseEvent} evt
- */
-const addElementHandler = function (evt) {
-  const clickedBtn = evt.target;
-  const addMenuElement = clickedBtn.parentNode;
-
-  const block = {
-    id: getRandomNumber(1000000),
-    type: clickedBtn.dataset.type,
-    content: clickedBtn.dataset.type === 'img' ? DEFAULT_IMG : clickedBtn.dataset.type,
-    container: clickedBtn.dataset.container,
-  };
-
-  renderElement(block);
-
-  addMenuElement.classList.add('hidden');
-
-  currentData.push(block);
-};
-
-/**
- * Обработчик изменения сетки сайта
- * @param {MouseEvent} evt
- */
 const changeLayoutHandler = function (evt) {
   const newLayout = evt.target.value;
-
-  switchLayout(newLayout);
-};
-
-/**
- * Метод переключения сетки сайта
- * @param {string} layoutType
- */
-const switchLayout = function (layoutType) {
   const layoutElement = document.querySelector('.layout');
   layoutElement.classList.remove('layout--landing');
   layoutElement.classList.remove('layout--blog');
   layoutElement.classList.remove('layout--shop');
-  layoutElement.classList.add('layout--' + layoutType);
+  layoutElement.classList.add('layout--' + newLayout);
+};
+document.querySelector('.grid-select').addEventListener('change', changeLayoutHandler);
+
+
+
+
+
+// ********************* 13 *********************
+/* Обработчик удаления элемента: .remove(), .classList.contains, .classList.add */
+ const buttonDeleteHandler = function (evt) {
+  // найти родителя кнопки
+  const element = evt.target.parentNode;
+  //найти *__elements-wrapper
+  const wrapper = element.parentNode;
+  //найти часть сайта - шапка, контент или подвал
+  const block = wrapper.parentNode;
+
+  // удалить элемент
+  element.remove();
+
+  const wrapperItems = wrapper.querySelectorAll('.element');
+  // когда в *__elements-wrapper нет элементов, 
+  // добавить класс *--empty его родителю
+  if (wrapperItems.length === 0) {
+     
+    if (block.classList.contains('header')) {
+      block.classList.add('header--empty');
+    } 
+
+    if (block.classList.contains('content')) {
+      block.classList.add('content--empty');
+    }
+
+    if (block.classList.contains('footer')) {
+      block.classList.add('footer--empty');
+    }
+  }
 };
 
-init();
+
+// ********************* 14 *********************
+/* Обработчик изменения контента: .tagName, .src, .textContent */
+const editContentHandler = function (evt) {
+  // найти на что кликнули
+  const editedElement = evt.target;
+  //console.log(editedElement.tagName);
+  // сохраняем текущее содержимое текста или адрес до картинки
+  let currentValue;
+
+  // если это картинка, то берем данные из атрибута src
+  if (editedElement.tagName === 'IMG') {
+    currentValue = editedElement.src;
+  } else {
+  // если текст, то из textContent
+    currentValue = editedElement.textContent;
+  }
+
+  // показывает окно промпт, значение по-умолчанию - текущий текст
+  // сохраняем новые данные в переменную
+  const newValue = window.prompt('Вы хотите поменять значение?', currentValue);
+
+  //заменяем данные на новые, если они не пустые
+  if (newValue) {
+    // если клик на картинке, то будем менять атрибут src
+    if (editedElement.tagName === 'IMG') {
+      editedElement.src = newValue;
+    } else {
+    // если текст, то запишем в textContent
+      editedElement.textContent = newValue;
+    }
+  }
+};
+
+
+// ********************* урок 12  *********************
+/* добавления элемента на страницу при клике на кнопку из панели элементов */
+const addElementHandler = function (evt) {   
+  // найти нажатую кнопку
+  const clickedBtn = evt.target; 
+
+  // найти ее родителя - всю панель элементов
+  const addMenuElement = clickedBtn.parentNode;  
+  // скрыть панель элементов
+  addMenuElement.classList.add('hidden'); 
+
+  // найти тип блока для подстановки в класс шаблона
+  const blockType = clickedBtn.dataset.type;
+  //console.log(blockType);
+
+  // найти инфо о контейнере и класс куда добавлять скопированный тепмлейт
+  const blockContainer = clickedBtn.dataset.container;
+  //console.log(blockContainer);
+  //console.log('.' + blockContainer + '__elements-wrapper');
+
+  // найти и скопировать шаблон
+  const templateElement = document.querySelector('#' + blockType + '-template').content.cloneNode(true);
+  const blockElement = templateElement.querySelector('.element');
+  //console.log(blockElement);
+
+  // вставить элемент в свой контейнер
+  const containerWrapperElement = document.querySelector('.' + blockContainer + '__elements-wrapper');
+  containerWrapperElement.append(blockElement);
+
+  //удаление *--empty
+  if (blockContainer.includes('content')) {
+    containerWrapperElement.parentElement.classList.remove('content--empty');
+  } else {
+    containerWrapperElement.parentElement.classList.remove(blockContainer + '--empty');
+  }
+
+
+  // ********************* урок 13  *********************
+  blockElement.querySelector('.delete-btn').addEventListener('click', buttonDeleteHandler);
+
+  // ********************* урок 14  *********************
+  blockElement.querySelector('.template-content').addEventListener('dblclick', editContentHandler); // ,true ????
+
+};
+
+// ********************* урок 12  *********************
+const chooseButtonElements = document.querySelectorAll('.choose-elem__btn');
+chooseButtonElements.forEach(function (item) {
+  return item.addEventListener('click', addElementHandler);
+});
+
+
